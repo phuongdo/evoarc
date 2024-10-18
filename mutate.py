dfdf9e9c44a54165f0da0c3524f499859d9dd342
hu.po <hugo.p.cmu@gmail.com>
2024-10-18 11:20:04 -0500

    asd

--- mutate.py
+++ mutate.py
@@ -3,8 +3,8 @@ import argparse
 from evolve import export, mutate, Morph
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--morph", help="protomorph to mutate", default="base")
-parser.add_argument("--mutation", help="filename of prompt to mutate with", default="open_ended")
+parser.add_argument("--morph", help="protomorph to mutate", default="base.adam")
+parser.add_argument("--mutation", help="filename of prompt to mutate with", default="rewrite_model")
 args = parser.parse_args()
 
 neomorph = mutate(Morph(0, args.morph), args.mutation)
